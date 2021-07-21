6b680de0a8b130b9084ec694863fa2f5c97a53bf
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2021-07-21 14:09:18 -0400

    Modulos parte 8

---
+++ ext_super/treasury_internal/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name":"Extensión para Transferencias Internas",
+    "description":"Permite añadir transferencias internas.",
+    "author":"Oasis Consultora",
+    "depends":['account'],
+    "data":[
+        'views/views.xml',
+    ]
+}
