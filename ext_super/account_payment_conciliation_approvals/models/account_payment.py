f3cce5ee00cdf5614232e81e57e28ae970fd1a72
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2021-10-18 16:27:57 -0400

    zzz

--- ext_super/account_payment_conciliation_approvals/models/account_payment.py
+++ ext_super/account_payment_conciliation_approvals/models/account_payment.py
@@ -11,7 +11,29 @@ class account_payment(models.Model):
     is_approved = fields.Boolean(default=False)
     state = fields.Selection(selection_add=[('to_approve', 'To be Approved'), ('approved', 'Approved'), ('refused', 'Refused')])
     
-    
+    def post(self):
+        xfind = self.env['approval.request'].search([('conciliation_id', '=', self.id)])
+        is_company =  self.env['res.company'].search([('partner_id', '=', self.partner_id.id)])
+
+        status = xfind.request_status
+        if len(xfind) > 0:
+            if status == 'approved':
+                self.is_approved = True
+            else:
+                self.is_approved = False
+        elif len(is_company) > 0:
+            self.is_approved = True
+        elif self.payment_method_id.aprobacion_requerida == False:
+            self.is_approved = True
+        else:
+            self.is_approved = False
+
+        if self.is_approved:
+            super(account_payment, self).post()
+        elif status == 'refused':
+            self.state = 'refused'
+        else:
+            raise ValidationError(_("Cannot confirm until an approval request is approved for this payment."))
 
     def approved_post(self):
         xfind = self.env['approval.request'].search([('conciliation_id', '=', self.id)])
