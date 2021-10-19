0baea0c02dd0914861239da65cee7039587cd6d5
dgdev27 <odigregorio@oasisconsultora.com>
2021-10-19 10:34:31 -0400

    ajustes

--- ext_super/ext_generico_block_stock/model/sale_order_inherit.py
+++ ext_super/ext_generico_block_stock/model/sale_order_inherit.py
@@ -10,34 +10,14 @@ class SaleOrderLine(models.Model):
 
     stock=fields.Float(compute='_compute_stock_mano')
 
-    @api.onchange('product_id')
     def _compute_stock_mano(self):
         for selff in self:
             selff.stock=selff.product_id.qty_available
 
-    @api.onchange('product_id','product_uom_qty')
-    #@api.depends('product_id')
-    def valida(self):
-        #raise UserError(_("Prueba"))
-        if not self.product_id.id:
-            pass
-        else:
-            if self.product_id.qty_available>0:
-                if self.product_id.qty_available>=self.product_uom_qty:
-                    if self.product_uom_qty>0:
-                        pass
-                    else:
-                        raise UserError(_("La cantidad seleccionada no debe ser igual a cero"))
-                else:
-                    raise UserError(_("La cantidad a vender del producto %s no puede ser mayor al stock actual")%self.product_id.name)
-            else:
-                raise UserError(_("El producto %s no puede ser vendido con stock cero o negativo")%self.product_id.name)
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
 
-#class SaleOrder(models.Model):
-    #_inherit = 'sale.order'
-
-
-    """def action_confirm(self):
+    def action_confirm(self):
         super().action_confirm()
         for det in self.order_line:
             if det.product_id.qty_available>0:
@@ -49,4 +29,4 @@ class SaleOrderLine(models.Model):
                 else:
                     raise UserError(_("La cantidad a vender del producto %s no puede ser mayor al stock actual")%det.product_id.name)
             else:
-                raise UserError(_("El producto %s no puede ser vendido con stock cero")%det.product_id.name)"""
+                raise UserError(_("El producto %s no puede ser vendido con stock cero")%det.product_id.name)
