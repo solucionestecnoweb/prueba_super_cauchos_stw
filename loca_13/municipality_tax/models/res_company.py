acd3cd360605904042ecc2a88eb92ad6e16687ca
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2021-11-02 16:37:56 -0400

    Municipality_tax

---
+++ loca_13/municipality_tax/models/res_company.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+from odoo import api, fields, models, _ 
+
+
+
+
+
+class Company(models.Model):
+    _inherit = 'res.company'
+
+
+    muni_wh_agent = fields.Boolean(string='Retention agent', help='True if your partner is a municipal retention agent')
+    purchase_jrl_id = fields.Many2one('account.journal', string='Purchase journal')
+    sale_jrl_id = fields.Many2one('account.journal', string='Sales journal')
+    account_ret_muni_receivable_id = fields.Many2one('account.account', string='Cuenta Retencion Clientes')
+    account_ret_muni_payable_id = fields.Many2one('account.account', string='Cuenta Retencion Proveedores')
+    nit = fields.Char(string='NIT', help='Old tax identification number replaced by the current RIF')
+    econ_act_license = fields.Char(string='License number', help='Economic activity license number')
+    nifg = fields.Char(string='NIFG', help='Number assigned by Satrin')
